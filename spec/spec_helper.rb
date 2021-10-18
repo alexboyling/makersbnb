84237cc682ca24279d011a794fdfe1d8497798cc
MattW <mattnwarnock@yahoo.co.uk>
2021-10-18 16:45:35 +0100

    setup

---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'capybara'
+require 'capybara/rspec'
+require 'pry'
+require 'rspec'
+require 'sinatra/reloader'
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [
+    SimpleCov::Formatter::Console,
+    SimpleCov::Formatter::HTMLFormatter
+  ]
+)
+SimpleCov.start
+
+RSpec.configure do |config|
+  config.after(:suite) do
+    puts
+    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+  end
+end
